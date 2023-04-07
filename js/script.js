ce99e85176aedb8cc56df2c0edbcb50b3d58ca46
Luckyyy <jaaniii@12345678@gmail,com>
2023-04-07 22:09:40 +0530

    added

---
+++ js/script.js
@@ -0,0 +1,67 @@
+var popup = document.getElementById("popup");
+var popupButton = document.getElementById("popupButton");
+var closeButton = document.getElementById("closeButton");
+
+popupButton.addEventListener("click", function() {
+    popup.showModal();
+});
+
+closeButton.addEventListener("click", function() {
+    popup.close();
+});
+
+var popup1 = document.getElementById("popup1");
+var popupButton1 = document.getElementById("popupButton1");
+var closeButton1 = document.getElementById("closeButton1");
+
+popupButton1.addEventListener("click", function() {
+    popup1.showModal();
+});
+
+closeButton1.addEventListener("click", function() {
+    popup1.close();
+});
+var popup2 = document.getElementById("popup2");
+var popupButton2 = document.getElementById("popupButton2");
+var closeButton2 = document.getElementById("closeButton2");
+
+popupButton2.addEventListener("click", function() {
+    popup2.showModal();
+});
+
+closeButton2.addEventListener("click", function() {
+    popup2.close();
+});
+var popup3 = document.getElementById("popup3");
+var popupButton3 = document.getElementById("popupButton3");
+var closeButton3 = document.getElementById("closeButton3");
+
+popupButton3.addEventListener("click", function() {
+    popup3.showModal();
+});
+
+closeButton3.addEventListener("click", function() {
+    popup3.close();
+});
+var popup4 = document.getElementById("popup4");
+var popupButton4 = document.getElementById("popupButton4");
+var closeButton4 = document.getElementById("closeButton4");
+
+popupButton4.addEventListener("click", function() {
+    popup4.showModal();
+});
+
+closeButton4.addEventListener("click", function() {
+    popup4.close();
+});
+var popup5 = document.getElementById("popup5");
+var popupButton5 = document.getElementById("popupButton5");
+var closeButton5 = document.getElementById("closeButton5");
+
+popupButton5.addEventListener("click", function() {
+    popup5.showModal();
+});
+
+closeButton5.addEventListener("click", function() {
+    popup5.close();
+});
